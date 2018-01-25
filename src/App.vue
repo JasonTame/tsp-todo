1f9416783252ba4ddad59011da9eccf6af3cc353
Jason Tame <jason@socialproject.co.za>
2018-01-25 11:27:25 +0200

    Adding To Do App Front-end code

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,154 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<div id="app">
+  <h1 class="text-center title">Simple Task Manager</h1>
+
+
+  <div class="row">
+    <div class="taskList col-xs-12">
+
+      <div class="taskButton">
+
+        <input id="addTaskInput" value="Add task name here" v-model="addTask" /> <button class="btn btn-info" @click="addNewTask">Add Task</button>
+
+      </div>
+      <br>
+      <ul>
+        <li v-for="(task, key) in tasks"><input type="checkbox" v-model="task.completed" /> {{task.description}}
+          <button class="btn btn-danger" @click="deleteTask(key)"><i class="fa fa-trash" aria-hidden="true"></i></button> </li>
+      </ul>
+
+
+    </div>
+
   </div>
+
+  <div class="row">
+    <div class="col-xs-6 taskList">
+      <h4>Incomplete tasks </h4>
+      <ol>
+        <li v-for="task in incompleteTasks" v-text="task.description"></li>
+      </ol>
+    </div>
+    <div class="col-xs-6 taskList">
+      <h4>Complete tasks </h4>
+      <ol>
+        <li v-for="task in completeTasks" v-text="task.description"></li>
+      </ol>
+    </div>
+  </div>
+  </ul>
+</div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      addTask: "",
+      tasks: [{
+          description: "Take out trash",
+          completed: true
+        },
+        {
+          description: "Do washing",
+          completed: false
+        },
+        {
+          description: "Iron clothes",
+          completed: true
+        },
+        {
+          description: "Grocery shopping",
+          completed: false
+        }
+      ]
+    }
+  },
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter(task => !task.completed);
+    },
+
+    completeTasks() {
+      return this.tasks.filter(task => task.completed);
+    }
+  },
+  methods: {
+    addNewTask() {
+      this.tasks.push({
+        description: this.addTask,
+        completed: false
+      });
+
+      this.addTask = "";
+    },
+
+    deleteTask(key) {
+      this.tasks.splice(this.tasks.indexOf(key), 1);
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0 auto;
+  width: 100%;
+  background-color: #0f0c0e;
 }
 
-h1, h2 {
-  font-weight: normal;
+.title {
+  padding-top: 20px;
 }
+
+h1,
+h2,
+h3,
+h4 {
+  color: #f7ab1d;
+}
+
+p,
+li {
+  color: #fff;
+}
+
+
 
 ul {
-  list-style-type: none;
-  padding: 0;
+  list-style: none;
+  padding: 0px 10px 0px 10px
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+  position: relative;
+  padding: 10px 0px 10px 0px;
 }
 
-a {
-  color: #42b983;
+li>button {
+  position: absolute;
+  right: 0;
+}
+
+.taskButton {
+  margin: 0 auto;
+  text-align: center;
+  padding: 10px;
+}
+
+.taskButton button {}
+
+button:hover {
+  cursor: pointer;
+}
+
+.taskButton {
+  margin: 0 auto;
+}
+
+.taskList {
+  margin: 0 auto;
+  padding: 10px;
 }
 </style>
